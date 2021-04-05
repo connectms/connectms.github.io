b2b0fbd4792b9f4f66624c3596a7a164e861d682
Rifqi Irsyadillah <rifirsyad@gmail.com>
2021-04-06 01:05:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+<title>Connect - Marketing Solutions</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://connectms.ga/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+<style>
+body, html {
+  height: 100%;
+  font-family: "Inconsolata", sans-serif;
+}
+
+.bgimg {
+  background-position: center;
+  background-size: cover;
+  background-image: url("https://img.connectms.ga/kantor.png");
+  min-height: 75%;
+}
+
+.menu {
+  display: none;
+}
+</style>
+<body>
+
+<!-- Links (sit on top) -->
+<div class="w3-top">
+  <div class="w3-row w3-padding w3-black">
+    <div class="w3-col s3">
+      <a href="#" class="w3-button w3-block w3-black">HOME</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="#about" class="w3-button w3-block w3-black">BACKGROUND</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="#story" class="w3-button w3-block w3-black">STORY</a>
+    </div>
+    <div class="w3-col s3">
+      <a href="https://career.connectms.ga" target="_blank" class="w3-button w3-block w3-black">LOWONGAN PEKERJAAN</a>
+    </div>
+  </div>
+</div>
+
+<!-- Header with image -->
+<header class="bgimg w3-display-container w3-grayscale-min" id="background">
+  <div class="w3-display-bottomleft w3-center w3-padding-large w3-hide-small">
+    <span class="w3-tag">Mulholland Intersection (Barat Parkir Downtown)</span>
+  </div>
+  <div class="w3-display-middle w3-center">
+    <span class="w3-text-white" style="font-size:90px"><img src="https://img.connectms.ga/connect.png"></span>
+  </div>
+  <div class="w3-display-bottomright w3-center w3-padding-large">
+    <span class="w3-text-white"></span>
+  </div>
+</header>
+
+<!-- Add a background color and large text to the whole page -->
+<div class="w3-sand w3-large">
+
+<!-- About Container -->
+<div class="w3-container" id="about">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-64"><span class="w3-tag w3-wide">BACKGROUND</span></h5>
+    <p><strong>NAMA KOMUNITAS:</strong> Connect - Marketing Solutions</p>
+    <p><strong>NAMA LEADER:</strong> Simon Toronto</p>
+    <p><strong>RAS KOMUNITAS:</strong> Bebas/Tidak terikat (Faction Legal)</p>
+    <img src="https://img.connectms.ga/5.png" style="width:100%;max-width:1000px">
+    <p>Connect merupakan perusahaan pertama di Los Santos yang bergerak sebagai jasa perantara transaksi antara penjual dengan pembeli dan berperan aktif sebagai pemecah masalah dalam sebuah penjualan. Connect memiliki berbagai divisi yaitu; Agen (Agency), Makelar (Broker), dan Komisioner (Commision Merchant). Divisi-divisi tersebut pada dasarnya diadakan untuk dapat memenuhi segala kebutuhan induk perusahaan.</p>
+    <br><p><center><strong>DIVISI</strong></center>
+        <ul><li>Agen adalah perantara perdagangan yang atas nama suatu perusahaan tertentu untuk menjualkan property/kendaraan. Agen menjual property/kendaraan tersebut dengan harga kesepakatan antara dirinya dan produsen. Agen akan mendapatkan keuntungan berdasarkan komisi atau pendapatan lain sesuai kesepakatan.</li>
+        <li>Makelar adalah perantara yang atas nama orang lain (pemberi kuasa) mencarikan property/kendaraan bagi pembeli dan atau menjualkan property/kendaraan bagi penjual. Makelar tidak ikut bertanggung jawab atas penyerahan barang pembayarannya. Tugasnya hanya memungkinkan penjual dan pembeli mengadakan perjanjian jual beli sendiri.</li>
+        <li>Komisioner adalah perantara dalam perdagangan seperti juga makelar. Ia bekerja atas namanya sendiri dan ikut bertanggung jawab sendiri atas tindakan yang dilakukan dalam mengadakan perjanjian jual beli. Untuk jasanya ia memperoleh komisi.</li></ul>
+    </p><br>
+    <div class="w3-panel w3-leftbar w3-light-grey">
+      <p><center><strong>S M A R T</strong></center>Seluruh aktivitas yang dijalankan oleh Connect.corp berlandaskan filosofi manajemen yang dirumuskan dengan akronim <b>SMART</b>:<br>
+        <ul><li><b>S</b>ervice yang berkualitas</li>
+        <li><b>M</b>anajemen yang berjiwa marketing</li>
+        <li><b>A</b>ntisipasi iklim bisnis</li>
+        <li><b>R</b>espect kepada pemakai jasa, pemegang saham dan karyawan; serta</li>
+        <li><b>T</b>oleransi dan keterbukaan.</li></ul>
+    </p>
+    </div><br>
+    <p><center><strong>Sistem Kerja</strong></center>
+        <ol><li>SISTEM HARGA MATI<br>Sistem ini tidak memungkinkan makelar memainkan harga dan mengambil untung lebih. Berapapun nilai transaksinya, dia hanya akan mendapat komisi sesuai perjanjian awal.</li>
+        <li>SISTEM HARGA INDUK<br>Pemilik barang telah menentukan harga patokan. Terserah makelar mau menjual berapa pada konsumen. Makelar bebas mencari keuntungan sendiri dan selisih harga yang telah ditetapkan dengan harga produk yang terjual. Makin mahal harganya, makin besar untungnya.</li>
+        <li>SISTEM BAYAR BERES<br>Pihak client akan membayar jika urusan yang diinginkannya sudah selesai dikerjakan oleh si penyedia jasa. Biasanya mensyaratkan DP di muka sebagai jaminan dan dibayar saat sudah selesai.</li>
+        <li>SISTEM CEPAT<br>Dalam sistem ini, pihak client dapat menjual kendaraan atau property nya langsung kepada pihak connect dengan harga murah. Sistem ini berguna untuk client yang ingin menjual cepat tapi masih harga wajar</li></ol>
+    </p><br>
+    <p><center><strong>Kode Etik dalam Dunia Makelar</strong></center>
+        Jika kamu ingin menekuni bidang jasa perantara barang/property alias makelar, kamu wajib tahu etika berikut:<br>
+        <ol><li>PRINSIP KEJUJURAN<br/>Ini kode etik paling penting. Prinsip kejujuran menjadi pondasi utama yang harus dimiliki oleh setiap makelar. Benar, insentif pendapatan dari komisi, provisi atau fee penjualan adalah tujuan utama dari bisnis ini. Tapi motif ekonomi ini tidak boleh sampai bikin kita mementingkan keuntungan sesaat lalu mengaburkan nilai-nilai kejujuran</li>
+        <li>TIDAK MENERIMA BARANG TERLARANG<br>Kode etik makelar kedua adalah legalitas produk yang dijual. Broker yang baik hanya menjadi perantara untuk jenis produk dan jasa yang legal secara hukum. Tidak dilarang peredarannya oleh pemerintah, tidak bertentangan dengan norma agama, adat istiadat setempat serta tidak dalam status sengketa</li>
+        <li>MEMBUAT PERJANJIAN DARI AWAL SECARA JELAS<br>Kode etik ketiga adalah membuat perjanjian pra-transaksi. Agar terhindar dari selisih paham kelak, sebaiknya membuat perjanjian yang jelas. Jika perlu perjanjian tertulis hitam di atas putih. Ini penting guna melindungi kepentingan semua pihak. Jangan sampai ada celah yang bisa dimanfaatkan pihak tak bertanggung jawab</li>
+        <li>TIDAK BOLEH MENJELEK-JELEKKAN KOMPETITOR<br>Demi alasan apapun, tidak etis menjatuhkan dan menjelek-jelekkan kompetitor, baik itu broker perorangan maupun agen lain. Karena selain bisa menimbulkan fitnah, juga bisa mencoreng profesi makelar di mata masyarakat umum.</li></ol></p>
+    <br><p>TUJUAN KODE ETIK
+        <ol><li>Memberi batasan apa yang boleh dan tidak boleh dilakukan dalam menjalankan aktifitas makelaran</li>
+        <li>Mengedukasi para broker agar menjadi seorang mediator yang berakhlak, beretika baik, bermoral dan bersikap profesional dalam melakukan aktifitas percaloan.</li>
+        <li>Mencegah supaya makelar atau calo tidak berbuat nakal serta melakukan praktek curang, nakal, tidak fair yang bisa merugikan orang lain maupun diri sendiri.</li>
+        <li>Menjaga agar persaingan berlangsung secara sehat.</li>
+        <li>Menjaga nama baik/citra profesi makelar di mata masyarakat.</li>
+        <li>Menjaga asas kerukunan, kebersamaan, solidaritas, rasa setia kawan, saling menghormati/menghargai antar sesama makelar agar tidak bentrok memperebutkan ‘proyek’ di lapangan.</li>
+        <li>Menjaga kepercayaan pelanggan agar mau menggunakan jasa perantara untuk menghubungkan penjual dengan pembeli.</li>
+        <li>Memberikan jaminan prinsip saling menguntungkan antara makelar dengan pemilik barang/jasa dan juga pembeli. Sehingga kesuksesan satu pihak, berarti sukses bersama.</li>
+        <li>Memberikan sanksi secara hukum dan moral bagi yang melanggar kode etik tersebut.</li></ol>
+    </p>
+  </div>
+</div>
+
+<!-- Story Container -->
+<div class="w3-container" id="story">
+  <div class="w3-content" style="max-width:700px">
+ 
+    <h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">STORY</span></h5>
+    <img src="https://img.connectms.ga/2.png" style="width:100%;max-width:1000px;">
+    <p>Simon Toronto memulai kisahnya di Los Santos dengan membuka sebuah perusahaan yang diharapkan mampu memberikan dampak positif bagi masyarakat luas, ia datang karena melihat upaya makelar dalam transaksi property dan kendaraan sangat dibutuhkan saat ini. Mengingat, Los Santos saat ini sedang marak sekali terjadi kecurangan dalam hal jual beli hingga merembet ke penipuan. Simon berharap, upaya yang ia lakukan ini berbuah manis dan sukses</p>
+    <br>
+    
+    <div class="w3-row w3-center w3-card w3-padding">
+      <a href="javascript:void(0)" onclick="openMenu(event, 'surpeng');">
+        <div class="w3-col s6 tablink">Surat Pengantar</div>
+      </a>
+      <a href="javascript:void(0)" onclick="openMenu(event, 'siup');" id="myLink">
+        <div class="w3-col s6 tablink">SIUP</div>
+      </a>
+    </div>
+
+    <div id="surpeng" class="w3-container menu w3-padding-48 w3-card">
+      <p>Dalam membuka sebuah perusahaan, kita memerlukan SIUP sebagai alat pengesahan yang diberikan oleh pemerintah sehingga dalam kegiatan usaha nantinya tidak terjadi masalah.</p>
+      <img src="https://img.connectms.ga/mta-screen_2018-01-18_20-21-23.png" style="width:100%;max-width:1000px;">
+      <p>Dalam hal ini, Connect tentu mengurus surat izin tersebut dengan pemerintah. Hanya saja sebelum sampai ke pemerintah, kita harus meminta surat pengantar dari LSPD terlebih dahulu.</p>
+    </div>
+
+    <div id="siup" class="w3-container menu w3-padding-48 w3-card">
+      <p>Beberapa hari kemudian setelah selesai mengurus surat pengantar dari LSPD, Connect mengurus SIUP dengan mengikuti berbagai persyaratan dalam membuka sebuah perusahaan. Tentu sebelum datang ke City hall, Connect sudah memantapkan diri bahwa perusahaan ini siap dan telah mengikuti segala persyaratan yang ada.</p>
+      <img src="https://img.connectms.ga/mta-screen_2018-01-30_22-27-10.png" style="width:100%;max-width:1000px;">
+      <p>Setelah selesai ditandatangani, ini artinya Connect sudah diberikan izin oleh pemerintah untuk mengoperasikan tugasnya sebagai perantara atau makelar dalam transaksi. Connect berharap, mampu memberikan kontribusi serta pengaruh yang besar</p>
+      <img src="https://img.connectms.ga/Untitled.png" style="width:100%;max-width:1000px;">
+    </div>  
+    
+    <br><p>Saya bersedia untuk mengikuti semua sistem yang ada pada server ILRP meskipun jika ada perubahan akan saya patuhi. Dengan ini saya <strong>Budi Tiawarman</strong> menyetujui semua peraturan dan sistem yang telah ILRP tentukan.</p>
+    <p class="w3-text-grey">Last edited by audisasuke31 on Sun Feb 18, 2018 1:57 pm, edited 20 times in total.</p>
+  </div>
+</div>
+
+<!-- In-Game Container -->
+<div class="w3-container" id="where" style="padding-bottom:32px;">
+  <div class="w3-content" style="max-width:700px">
+    <h5 class="w3-center w3-padding-48"><span class="w3-tag w3-wide">In-Game</span></h5>
+    <p>Mulholland Intersection (Barat Parkir Downtown) - F11 > Dealer Connect (Cv)</p>
+    <img src="https://img.connectms.ga/showroom.png" class="w3-image" style="width:100%">
+    <p><span class="w3-tag">FYI!</span> Kami sedang membuka lowongan pekerjaan, daftar sekarang <a href="https://career.connectms.ga" target="_blank">di sini</a>.</p>
+  </div>
+</div>
+
+<!-- End page content -->
+</div>
+
+<!-- Footer -->
+<footer class="w3-center w3-light-grey w3-padding-48 w3-large">
+  <p><a href="https://forum.indoliferoleplay.com/viewforum.php?f=98" target="_blank">
+    <img src="https://img.connectms.ga/ilrp.png" class="w3-image" style="width:50px"></a></p>
+</footer>
+
+<script>
+// Tabbed Menu
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-dark-grey", "");
+  }
+  document.getElementById(menuName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-dark-grey";
+}
+document.getElementById("myLink").click();
+</script>
+
+</body>
+</html>
